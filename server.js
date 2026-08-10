a46a171fbb3fab692ee5a92ca07f3029871403fd
Sabih <msabihapple@gmail.com>
2026-08-10 15:03:15 +0500

    Server File Changed

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const http = require('node:http');
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+const app = express();
+
+  
+dotenv.config();
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/", (req, res) => {
+    res.send("Server is working");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const PORT = process.env.PORT || 3007;
+app.listen(PORT,() => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
